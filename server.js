0fad78571ce773a0a8c59eead23d02153c0f8977
trevor ferguson <trevorjadeferguson@gmail.com>
2022-12-31 03:22:58 -0800

    change port env variable to match EBS port var

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb')
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(express.json());
