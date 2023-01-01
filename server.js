8ec766b285fd0e9cc79a051e5fbcdf938d2ea4ae
trevor ferguson <trevorjadeferguson@gmail.com>
2023-01-01 00:07:26 -0800

    fix cors headers errors, add middleware before routes

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
+// account for incoming json
 app.use(express.json());
+
+// cors headers issues, placing before routes should solve
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // mongoose cloud db setup here
 mongoose.set("strictQuery", false)
